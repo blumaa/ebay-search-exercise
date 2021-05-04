3fbee14bf3389898fdef66d158174e97892aa7b6
Aaron Blum <blumaa@gmail.com>
2021-05-04 17:27:23 +0200

    final commit

--- src/Gallery.js
+++ src/Gallery.js
@@ -4,12 +4,12 @@ import Filter from './components/Filter'
 import SearchResultsList from './components/SearchResultslist'
 
 const Gallery = () => {
-  const [ isLoading, setIsLoading ] = React.useState(false)
+  const [ isLoading, setIsLoading ] = React.useState(true)
   const [ search, setSearch ] = React.useState(null)
   const [ searchResults, setSearchResults ] = React.useState(false)
   const [ errors, setErrors ] = React.useState(null)
 
-  const url = `https://cors-anywhere.herokuapp.com/https://svcs.sandbox.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=WandoInt-217b-42d8-a699-e79808dd505e&keywords=harry%20potter&RESPONSE-DATA-FORMAT=JSON`
+  const url = `https://cors-anywhere.herokuapp.com/https://svcs.sandbox.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=WandoInt-217b-42d8-a699-e79808dd505e&keywords=${search}&RESPONSE-DATA-FORMAT=JSON`
 
   async function fetchData() {
     setIsLoading(true)
@@ -17,8 +17,13 @@ const Gallery = () => {
     res
       .json()
       .then(res => {
+        console.log('response', res.findItemsByKeywordsResponse[0])
         setSearchResults(res.findItemsByKeywordsResponse[0].searchResult[0].item)
-        setIsLoading(false)
+        if (res.findItemsByKeywordsResponse[0].searchResult[0].item.length > 0) {
+          setIsLoading(false)
+        } else {
+          setErrors("couldn't find anything")
+        }
       })
       .catch(err => setErrors(err));
   }
@@ -28,16 +33,11 @@ const Gallery = () => {
      fetchData() 
   },[search])
 
-
-  console.log('sesarch results', searchResults)
-  console.log('errors', errors)
-
   return (
-    <div className="gallery">
-      Ebay Search Exercise
+    <div className="Gallery">
+      <div className="Gallery__title">Ebay Search Exercise</div>
       <SearchBar setSearch={setSearch} />
-      <Filter />
-      {!isLoading && <SearchResultsList results={searchResults} isLoading={isLoading} />}
+      {!isLoading ?  <SearchResultsList searchResults={searchResults} isLoading={isLoading} /> : "loading"}
     </div>
   );
 }
--- src/components/SearchResultslist.jsx
+++ src/components/SearchResultslist.jsx
@@ -1,22 +1,28 @@
-import React from 'react'
+import React from "react";
 
+const ListEntry = (item) => {
+  return (
+    <div key={item.entry.itemId[0]} className="SearchResultslist__entry">
+      <div>{item.entry.title[0]}</div>
+      <div></div>
+    </div>
+  );
+};
 
-const ListEntry = item => {
-    console.log('list entry', item)
-    return(
-        <div>{item.title}</div>
-    )
-}
+const SearchResultsList = ({ searchResults, isLoading }) => {
 
-const SearchResultsList = ({results, isLoading}) => {
+  return (
+    <div className="SearchResultslist">
+      <div className="SearchResultslist__header">
+        <div>Item Name</div>
+        <div>Item Price</div>
+      </div>
+      <div className="SearchResultslist__list">
+        {!isLoading &&
+          searchResults.map((entry) => <ListEntry entry={entry} />)}
+      </div>
+    </div>
+  );
+};
 
-    console.log('search results', isLoading, results)
-
-    return (
-        <div>
-            {results.map(item=><ListEntry />) }
-        </div>
-    )
-}
-
-export default SearchResultsList
+export default SearchResultsList;
